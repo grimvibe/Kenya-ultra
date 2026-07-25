7e72525d45350d58f8244a68640e7ecaef4b7cea
Lucid dev <lawrencenjeri4@gmail.com>
2026-07-25 23:24:13 +0300

    Update clientActions.js

--- clientActions.js
+++ clientActions.js
@@ -8,21 +8,15 @@ export async function executeClientAction({
     switch (action) {
 
         case "recover_view_once":
-
-            // download and send media
-
             break;
 
         case "kick":
-
             break;
 
         case "promote":
-
             break;
 
         default:
-
             return false;
 
     }
